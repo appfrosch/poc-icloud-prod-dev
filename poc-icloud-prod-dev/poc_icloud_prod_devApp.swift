56683352adb6d7b75fa354294d0e5461b94cd677
Andreas Seeger <aseeger@dq-solutions.ch>
2025-08-08 11:26:28 +0200

    prepareDependencies

--- poc-icloud-prod-dev/poc_icloud_prod_devApp.swift
+++ poc-icloud-prod-dev/poc_icloud_prod_devApp.swift
@@ -5,10 +5,16 @@
 //  Created by Andreas Seeger on 08.08.2025.
 //
 
+import SharingGRDB
 import SwiftUI
 
 @main
 struct poc_icloud_prod_devApp: App {
+  init() {
+    prepareDependencies {
+      $0.defaultDatabase = try! AppDatabase.setup()
+    }
+  }
     var body: some Scene {
         WindowGroup {
             ContentView()
