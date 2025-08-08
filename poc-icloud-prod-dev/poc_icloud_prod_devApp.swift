17f965130340c1c59c1334b82b9cdea0f1a54407
Andreas Seeger <aseeger@dq-solutions.ch>
2025-08-08 11:09:41 +0200

    Initial Commit

---
+++ poc-icloud-prod-dev/poc_icloud_prod_devApp.swift
@@ -0,0 +1,17 @@
+//
+//  poc_icloud_prod_devApp.swift
+//  poc-icloud-prod-dev
+//
+//  Created by Andreas Seeger on 08.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct poc_icloud_prod_devApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
